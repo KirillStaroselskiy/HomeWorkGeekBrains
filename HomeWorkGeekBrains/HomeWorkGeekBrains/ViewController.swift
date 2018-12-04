d86c5989e3b15c36b8d6564d4933ec89c781b837
Kirill Staroselskiy <Staroselskiy.K@gmail.com>
2018-12-05 00:33:41 +0200

    start project

---
+++ HomeWorkGeekBrains/HomeWorkGeekBrains/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWorkGeekBrains
+//
+//  Created by Kirill on 05/12/2018.
+//  Copyright © 2018 Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
